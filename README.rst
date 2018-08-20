6efc662e2fddd6ef63d88f22bdbfe3a55784bec3
Denis Grigor <apprentice3d@gmail.com>
2018-08-20 15:06:19 -0400

    initial setup

---
+++ README.rst
@@ -0,0 +1,4 @@
+forge-api-python-client
+
+This Python SDK enables you to easily integrate the Forge REST APIs into your application,
+including OAuth, Data Management, Model Derivative, and Design Automation.
---
+++ setup.py
@@ -0,0 +1,24 @@
+name = 'forge-api-python-client'
+
+version = '0.1'
+
+description = 'SDK for easy integration of Autodesk Forge REST APIs in your project'
+
+long_description = '''This Python SDK enables you to easily integrate the Forge REST APIs into your application, 
+including OAuth, Data Management, Model Derivative, and Design Automation.'''
+
+url = 'https://github.com/apprentice3d/forge-api-python-client'
+
+author = 'Denis Grigor'
+
+author_email = 'denis.grigor@autodesk.com'
+
+classifiers = [
+                  'Development Status :: 1 - Planning',
+                  'Intended Audience :: Developers',
+                  'Environment :: Web Environment',
+                  'Topic :: Software Development :: Libraries :: Python Modules',
+                  'License :: OSI Approved :: MIT License',
+                  # 'Programming Language :: Python :: 2.7',
+                  'Programming Language :: Python :: 3',
+              ]
